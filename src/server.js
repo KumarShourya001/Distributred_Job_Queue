e4734bae9069c8997ddb3b41c7fc7a7a1291228e
Kumar Shourya <kshourya2005@gmail.com>
2026-08-30 16:16:12 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const app=express()
 app.use(express.json())
 
 const cors = (req, res, next) => {
-    // 1. Allow any origin to connect (Change '*' to a specific domain for production)
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); 
     
     next(); 
